807b87a090845622db1b16fa7ecfa1127566eb40
liuS <yxliufeifang@gmail.com>
2026-02-23 21:06:37 +0000

    Update access key check in guest.js

--- guest.js
+++ guest.js
@@ -1,12 +1,13 @@
 "use strict";
 const eE = new URLSearchParams(window.location.search);
-const DONT_TOUCH = eE.get('accessKEY3.1');
+const DONT_TOUCH = eE.get('k');
 const accessStatus = localStorage.getItem('accessStatusGranted');
-if (DONT_TOUCH == 'eee123eee') {
+if (DONT_TOUCH == 1234 ) {
     localStorage.setItem('accessStatusGranted', true)
 } 
 
 if (accessStatus == 'true') {
     window.location.href = "eee.html";
 }
+
 
